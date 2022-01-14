5001fd4a10d4fc5cae0436382f42bf366d89f5a4
Gaëtan Simon <gaetan.simon@jack.agency>
2022-01-14 16:14:26 +0100

    repro

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
 <template>
-  <Tutorial />
+  <Dummy />
 </template>
+
+<script>
+import Vue from 'vue'
+import Dummy from '~/components/Dummy'
+
+export default {
+  name: 'IndexPage',
+  mounted () {
+    try {
+      const Component = this.instanciateComponent()
+
+      // should throw "Cannot read properties of undefined (reading '_app')"
+      console.log('Content from <Dummy /> :', Component.configApp)
+    } catch (err) {
+      console.error('[Error from dynamic component in pages/index.vue]', err)
+    }
+  },
+  methods: {
+    instanciateComponent () {
+      const DummyComponent = Vue.extend(Dummy)
+
+      return new DummyComponent()
+    },
+  }
+}
+</script>
